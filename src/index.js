ff6a80e63cd2977d35a493b4a09adb207d3091cd
KK Chan <kk.chan@swivelsoftware.com>
2023-02-21 15:02:52 +0800

    add to support post request

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import {capture, latestCapture, latestCapturePage, showResults} from "./helpers.js";
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 9002;
 const app = express();
+app.use(express.json())
 app.get('/capture', capture);
+app.post('/capture', capture);
 if (showResults()) {
     app.get('/', latestCapturePage);
     app.get('/latest', latestCapture);
